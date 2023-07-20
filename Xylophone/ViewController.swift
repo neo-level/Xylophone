6938988ca7784eea83758467375eaabb7865de28
neo-level <frederik@neo-level.com>
2023-07-20 08:00:30 +0200

    created play sound function

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,17 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func KeyPressed(_ sender: UIButton) {
+        playSound()
     }
     
-    
-
+    func playSound() {
+        guard let path = Bundle.main.path(forResource: "C", ofType:"wav") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
